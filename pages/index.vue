bee4ebd64e6e2734c8a8ac71b9e16e3c8b2be866
Christian Carrillo Zúñiga <christiancazu@gmail.com>
2019-11-29 23:44:53 -0500

    init TS settings

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>NUXT SETTING WITH TS</h1>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class Index extends Vue {}
+</script>
